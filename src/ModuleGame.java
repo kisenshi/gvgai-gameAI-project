9297890b14983e3ce8e5ec2f2ea28c2b23786de7
kispygr <kispygr@gmail.com>
2017-02-20 01:17:03 +0000

    Added main java to run game, game description and level files. Basic logic, still need updates

---
+++ src/ModuleGame.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Cris
+ */
+
+import java.util.Random;
+import core.ArcadeMachine;
+
+public class ModuleGame {
+
+    public static void main(String[] args) {
+
+        // Available games:
+        String gamePath = "aimodule/";
+        String gameName = "cake";
+
+        // Other settings
+        boolean visuals = true;
+        int seed = new Random().nextInt();
+
+        // Game and level to play
+        String game = gamePath + gameName + ".txt";
+        String level1 = gamePath + gameName + "_lvl0.txt";
+
+        String recordActionsFile = null;// "actions_" + games[gameIdx] + "_lvl"
+        // + levelIdx + "_" + seed + ".txt";
+        // //where to record the actions
+        // executed. null if not to save.
+
+        // 1. This starts a game, in a level, played by two humans.
+        ArcadeMachine.playOneGameMulti(game, level1, recordActionsFile, seed);
+
+        // 2. This plays a game in a level by the controllers. If one of the
+        // players is human, change the playerID passed
+        // to the runOneGame method to be that of the human player (0 or 1).
+        //ArcadeMachine.runOneGame(game, level1, visuals, controllers, recordActionsFile, seed, 0);S
+    }
+}
